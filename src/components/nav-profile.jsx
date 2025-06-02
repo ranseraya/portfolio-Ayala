033f6d7dbbd307e539fa4623c313a54c5d564f1e
Ayala Septama Rahanda <143942469+ranseraya@users.noreply.github.com>
2025-06-02 20:47:16 +0700

    fix warning

--- src/components/nav-profile.jsx
+++ src/components/nav-profile.jsx
@@ -2,15 +2,45 @@
 
 import Image from "next/image";
 import { useTheme } from "next-themes";
+import { useState, useEffect } from "react";
 
 export function NavProfile({ user }) {
   const { theme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return (
+      <div className="mt-10 flex justify-center flex-col items-center gap-3">
+        <div className="rounded-md overflow-hidden scale-150 transition-all duration-500 group-data-[collapsible=icon]:scale-100">
+          <Image
+            src={user.image}
+            alt={`Foto Profil ${user.name}`}
+            width={100}
+            height={100}
+            className="object-cover"
+          />
+        </div>
+        <div className="mt-6 flex flex-col items-center truncate group-data-[collapsible=icon]:hidden">
+          <span className="orbitron text-sm font-medium leading-none text-foreground">
+            {user.name}
+          </span>{" "}
+          <span className="text-xs text-muted-foreground truncate">
+            {user.role}
+          </span>
+        </div>
+      </div>
+    );
+  }
 
   const currentImageSrc = theme === "dark" ? user.image : user.image2;
 
   return (
     <div className="mt-10 flex justify-center flex-col items-center gap-3">
-      <div className="rounded-full overflow-hidden border scale-150 transition-all duration-500 group-data-[collapsible=icon]:scale-100">
+      <div className="rounded-md overflow-hidden scale-150 transition-all duration-500 group-data-[collapsible=icon]:scale-100">
         <Image
           src={currentImageSrc}
           alt={`Foto Profil ${user.name}`}
@@ -20,7 +50,7 @@ export function NavProfile({ user }) {
         />
       </div>
       <div className="mt-6 flex flex-col items-center truncate group-data-[collapsible=icon]:hidden">
-        <span className="orbitron text-sm font-medium leading-none text-primary">
+        <span className="orbitron text-sm font-medium leading-none text-foreground">
           {user.name}
         </span>{" "}
         <span className="text-xs text-muted-foreground truncate">
